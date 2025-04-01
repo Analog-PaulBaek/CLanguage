b46fcc08adb889294a78f2492bf3583da4b157b4
AcornJavascript <qkdnf325@daum.net>
2025-04-01 22:11:42 +0900

    11~13

---
+++ 11. 입력 값을 변수에 저장하기/practice_scanf_three_integer.c
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    int num1 = 10;
+    float num2 = 1.234567f;
+    char num3 = 'a';
+
+    printf("%d\n", num1);
+    printf("%f\n", num2);
+    printf("%c\n", num3);
+
+    return 0;
+}
